a786b764bc9158edecc46c00fecfeec5717e9a11
christine <nyembe.mirhyne@gmail.com>
2024-08-21 11:10:08 +0100

    Initial commit

---
+++ src/test/java/com/microservice/authManager/AuthManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.authManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
